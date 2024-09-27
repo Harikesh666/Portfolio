03657bc9232d2e4bc9265c196cc65201fb8f9be6
Harikesh666 <mharikesh11@gmail.com>
2024-09-27 14:54:43 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const menuIcon = document.querySelector("#menu-icon");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links a");
+
+
+menuIcon.addEventListener("click", () => {
+    navLinks.classList.toggle('active');
+});
+
+window.addEventListener("beforeunload", () => {
+    const forms = document.getElementsByTagName("form");
+    for (const form of forms) {
+        form.reset();
+    }
+});
+
+links.forEach(link => {
+    link.addEventListener("click", () => {
+        navLinks.classList.remove('active'); // Close menu on link click
+    });
+});
+
+// menuIcon.addEventListener("click", () => {
+//     console.log("Menu icon clicked!"); // Check if this logs
+//     navLinks.classList.toggle('active');
+// });
+
